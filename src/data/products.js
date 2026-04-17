e967036a6ef2f0915242c45756852076aa219705
Seyhak <pheachseyhak8@gmail.com>
2026-04-17 22:04:47 +0700

    update again

--- src/data/products.js
+++ src/data/products.js
@@ -7,21 +7,23 @@ import ye from "../assets/ye.webp";
 import yk from "../assets/yk.webp";
 import o from "../assets/o.webp";
 import koo from "../assets/koo.webp";
-import one from "../assets/one.webp";
-import kong1 from "../assets/kong1.jpg";  
+import one from "../assets/one.webp"; 
+import hak from "../assets/hak.jpg";
+import ai from "../assets/ai.jpg"
 
 export const allProducts = [
-  { id: 1, name: "Classic Man T-Shirt", price: 200, category: "Men", image: "https://images.unsplash.com/photo-1521572267360-ee0c2909d518?q=80&w=1000", description: "High-quality cotton t-shirt for men, comfortable and durable." },
-  { id: 2, name: "Casual Girl Outfit", price: 25, category: "Women", image: y, description: "Stylish and comfortable casual wear for modern women." },
-  { id: 3, name: "Floral Summer Dress", price: 20, category: "Women", image: ye, description: "Perfect floral dress for your summer vacation." },
-  { id: 4, name: "Kids Leather Boots", price: 55, category: "Kids", image: "https://i.pinimg.com/736x/64/d2/e4/64d2e4b261ace7c314ce661cadad4894.jpg", description: "Strong and stylish leather boots for active kids." },
-  { id: 5, name: "Black Luxury Dress", price: 55, category: "Women", image: kk, description: "Elegant black dress, ideal for parties and evening events." },
-  { id: 6, name: "White Fashion Top", price: 40, category: "Women", image: ok, description: "A trendy white top that matches with any outfit." },
-  { id: 7, name: "Pink Party Dress", price: 50, category: "Women", image: pic11, description: "Beautiful pink dress for special celebrations." },
-  { id: 8, name: "Modern Graphic Tee", price: 18, category: "Women", image: qr, description: "Creative graphic design tee for a unique look." },
-  { id: 9, name: "girl party dress", price: 50, category: "Women", image: yk, description: "Creative graphic design tee for a unique look." },
-  { id: 10, name: "girl party dress", price: 50, category: "Women", image: o, description: "Creative graphic design tee for a unique look." },
-  { id: 11, name: "Modern Graphic black", price: 18, category: "Women", image: koo, description: "Creative graphic design tee for a unique look." },
-  { id: 12, name: "black Graphic Tee", price: 30, category: "Women", image: one, description: "black design tee for a unique look." },
-  { id: 13, name: "Boy Gen Z", price: 100, category: "Men", image: kong1, description: "Boy Gen Z." },
+  { id: 1, name: " Man T-Shirt", price: 400, category: "Men", image: hak, description: "High-quality cotton t-shirt for men, comfortable and durable." },
+  { id: 2, name: "Classic Man T-Shirt", price: 300, category: "Men", image: "https://images.unsplash.com/photo-1521572267360-ee0c2909d518?q=80&w=1000", description: "High-quality cotton t-shirt for men, comfortable and durable." },
+  { id: 3, name: "Casual Girl Outfit", price: 650, category: "Women", image: y, description: "Stylish and comfortable casual wear for modern women." },
+  { id: 4, name: "Floral Summer Dress", price: 300, category: "Women", image: ye, description: "Perfect floral dress for your summer vacation." },
+  { id: 5, name: "Kids Leather Boots", price: 80, category: "Kids", image: "https://i.pinimg.com/736x/64/d2/e4/64d2e4b261ace7c314ce661cadad4894.jpg", description: "Strong and stylish leather boots for active kids." },
+  { id: 6, name: "Black Luxury Dress", price: 500, category: "Women", image: kk, description: "Elegant black dress, ideal for parties and evening events." },
+  { id: 7, name: "White Fashion Top", price: 400, category: "Women", image: ok, description: "A trendy white top that matches with any outfit." },
+  { id: 8, name: "Pink Party Dress", price: 290, category: "Women", image: pic11, description: "Beautiful pink dress for special celebrations." },
+  { id: 9, name: "Modern Graphic Tee", price: 500, category: "Women", image: qr, description: "Creative graphic design tee for a unique look." },
+  { id: 10, name: "girl party dress", price: 350, category: "Women", image: yk, description: "Creative graphic design tee for a unique look." },
+  { id: 11, name: "girl party dress", price: 450, category: "Women", image: o, description: "Creative graphic design tee for a unique look." },
+  { id: 12, name: "Modern Graphic black", price: 560, category: "Women", image: koo, description: "Creative graphic design tee for a unique look." },
+  { id: 13, name: "black Graphic Tee", price: 600, category: "Women", image: one, description: "black design tee for a unique look." },
+  { id: 14, name: "Boy smart", price: 300, category: "Men", image: ai, description: "Boy Gen Z." },
 ];
