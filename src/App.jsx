301aa2e2bda8a880c529c80d07594c93351127a7
Seyhak <pheachseyhak8@gmail.com>
2026-04-27 19:10:04 +0700

    update products

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ import Shop from "./pages/Shop.jsx";
 import ProductDetail from "./pages/ProductDetail.jsx";
 import Cart from "./pages/Cart.jsx";
 import Checkout from "./pages/Checkout.jsx";
-import Login  from "./pages/Login.jsx";
-import Register  from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 import Contact from "./pages/Contact.jsx";
 import About from "./pages/About.jsx";
 import Review from "./components/ReviewSystem.jsx";
@@ -20,20 +20,28 @@ function App() {
   const [searchQuery, setSearchQuery] = useState(""); // បង្កើត State ស្វែងរក
   return (
     <CartProvider>
-      <Navbar setSearchQuery={setSearchQuery}/>
+      <Navbar setSearchQuery={setSearchQuery} />
       <Routes>
-        <Route path="/" element={<><Hero /><FeaturedProducts /></>} />
-        <Route path="/shop" element={<Shop searchQuery={searchQuery}/>} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <FeaturedProducts />
+            </>
+          }
+        />
+        <Route path="/shop" element={<Shop searchQuery={searchQuery} />} />
         <Route path="/product/:id" element={<ProductDetail />} />
         {/* 🔒 ការពារទំព័រ Cart: ចុចចូលបានលុះត្រាតែ Login រួច */}
-      <Route 
-        path="/cart" 
-        element={
-          <ProtectedRoute>
-            <Cart />
-          </ProtectedRoute>
-         }
-       />
+        <Route
+          path="/cart"
+          element={
+            <ProtectedRoute>
+              <Cart />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/login" element={<Login />} />
         <Route path="/Register" element={<Register />} />
